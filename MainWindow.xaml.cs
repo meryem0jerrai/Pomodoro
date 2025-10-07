12900af8b14e098cd8e7828266b7ccfd94da43be
Meryem <jerrai.meryem.mp@gmail.com>
2025-10-07 17:17:02 +0200

    pomodorro timer first implementation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,27 +11,65 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
+using System.Windows.Threading;
+using System.Diagnostics.Eventing.Reader;
+
 
 namespace Pomodoro
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    ///<summary>
+    ///Interaction logic for MainWindow.xaml
+    ///</summry>
     public partial class MainWindow : Window
     {
+        int n = 0; //champ de classe
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            lblTime.Content = "25:00";
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
 
-        private void TogglePress(object sender, RoutedEventArgs e)
+        void timer_Tick(object? sender, EventArgs e)
         {
-            var currentDate = DateTime.Now;
-            if (!string.IsNullOrWhiteSpace(timerValue.Text))
+            string[] parts = lblTime.Content.ToString().Split(':');
+            int minutes = int.Parse(parts[0]);
+            int seconds = int.Parse(parts[1]);
+
+            if (minutes == 0 && seconds == 0)
             {
-                timerValue.Clear();
+                if (n==0)
+                {
+                    lblTime.Content = "00:00";
+                    MessageBox.Show("Time's up! Take a break.");
+                    n++;
+                    // Fin de la période de travail
+                }
+                return;
+                
+                  
             }
+
+            if (seconds==0)
+            {
+                if (minutes > 0)
+                    {
+                    minutes--;
+                    seconds = 59;
+                }
+            }
+            else 
+                {
+                seconds--;
+            }
+            lblTime.Content = $"{minutes:D2}:{seconds:D2}";
+
         }
+
+        
     }
-    
+
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is my first time creating a desktop app, here's what I learned so far:
 
 1- Creating a windows interface using Windows Forms App (.NET Framework) in Visual Studio.
 
-2- Connexting the window controls (button, toggle) to the code behind (C#)
+2- Connecting the window controls (button, toggle) to the code behind (C#)
 
 3- Using event handlers to trigger actions when buttons are clicked or toggles are changed: In this case, the text box content is cleared if the toggle is checked.
 
